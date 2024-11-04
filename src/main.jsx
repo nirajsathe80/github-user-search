a8dc5f6f7b99a1efcf5841d2efe7563843cbabe7
nirajsathe80 <nirajsathe80@gmail.com>
2024-11-04 18:25:38 +0530

    strict mode add

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { UserProvider } from "./context/UserContext.jsx";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <UserProvider>
-    <App />
-  </UserProvider>
+  <StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </StrictMode>
 );
